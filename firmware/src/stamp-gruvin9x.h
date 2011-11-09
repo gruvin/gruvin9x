b35d11b1cf4a7dd94f39532e938672c82044c677
bsongis <bsongis@f6e3ec8c-4711-3139-51d9-7b347ef5a272>
2011-11-09 21:49:40 +0000

    stamp removed from the repo

--- firmware/src/stamp-gruvin9x.h
+++
@@ -1,6 +0,0 @@
-//Automatically generated file (Makefile) - do not edit
-#define DATE_STR "2011-11-07"
-#define TIME_STR "22:10:08"
-#define TAG_VERS 2-bsongis
-#define SVN_VERS "gruvin9x-trunk-r1199"
-#define BUILD_NUM 518
